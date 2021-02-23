061860f2e45d25248acc79ce3b78092ac57f303a
Numchik <maksim.nikitjuk@icloud.com>
2021-02-23 21:37:06 +0500

    DrinkInfo

---
+++ Food&Drink/DrinkTableViewController.swift
@@ -0,0 +1,47 @@
+//
+//  DrinkTableViewController.swift
+//  Food&Drink
+//
+//  Created by Максим Никитюк on 23.02.2021.
+//
+
+import UIKit
+
+class DrinkTableViewController: UITableViewController {
+    
+    let menuDrink = [
+    "beer REMIX SERIES: MAUKI", "RUSSIAN IMPERIAL STOUT V.IV",
+        "MACRO SERIES: SMOKY PEPPERS", "BRAVADA XMAS BOCK "
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+}
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return menuDrink.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "sellDrink", for: indexPath)
+        
+       
+        var content = cell.defaultContentConfiguration()
+        content.text = menuDrink[indexPath.row]
+        content.image = UIImage(named: menuDrink[indexPath.row])
+        content.secondaryText = "Цена: 500р"
+        
+        cell.contentConfiguration = content
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+            
+
+}
+
